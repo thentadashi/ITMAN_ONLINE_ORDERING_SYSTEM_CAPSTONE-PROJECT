10ef3219344dc46bc53f08e973d96aa52e6ea492
thentadashi <84229241+thentadashi@users.noreply.github.com>
2021-07-11 21:23:33 +0800

    Add files via upload

---
+++ terms.php
@@ -0,0 +1,34 @@
+   <!--Modal Log-out --> 
+
+            <div class="modal fade" id="logout" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body">
+
+ <h1>Terms And Condition</h1>
+
+<p>Online Ordering System – is software for products or any items that a specific shop that can sell their products or show their items through online.
+Online Ordering – this is a type of ordering where you can search or find an item through online even you are not going to a specific shop you can search or purchase an item. It is in demand right now one of factor is the Covid-19 pandemic, people are afraid to go to a physical store to buy an item even though you are at home you can browse an item.
+Ordering- this thing is important, if it is not existing you cannot purchase a specific products for anybody even it is for food or anything that is related to selling an item.
+IT Man - is a shop for computer parts and accessories or even CCTV’s located in Urdaneta , Pangasinan. This is also a place where products or items are sold and served to the client.
+Customers- They are the one who is surfing and finding a product in any Online Ordering System or even in physical stores. They are the who ask how much is that item, and how is that product.
+Seller- they are who ones who watch over the product and also sell the product.
+Administrator- They are the one who handled the system if there’s a product ordered online. This person is the one who also works for the inventory and what is going on to their shop.
+Paper- Based System- is where you use a paper to list the inventory and all the item or products sold.
+Cash on Delivery (COD)- this is when you pay the product to the delivery man itself ant not through online payment like Gcash, Paymaya, Paypal and etc.
+Cellphones/Smartphones - this particular thing is the one who uses of all customer to browse a specific items. When you don’t have a cellphone which means you are not capable of ordering an item online.
+Product - this is the things or materials what are the businesses or shop sells in their respective areas.
+Purchasing -  refers to a shop or business attempting to acquire items or products or services to finish the goal.
+Delivery – it is the time when your product or items coming from IT Man is transporting to the area where you put your location or destination.
+Internet – in order to access our system you need to have an Internet to surf and find any product you want to purchase.
+</p>
+
+</div>
+ 
+</div>
+</div>    
+</div>  
+                                      
+          <!--Logout end -->  
